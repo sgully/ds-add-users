de7cdd3a7ce0099e01602541494dd38c3f75e384
rmcm <rex.mcmaster@gmail.com>
2011-10-19 15:23:48 +1100

    Add new fields - for group and hostname

--- ds-add-users.sh
+++ ds-add-users.sh
@@ -17,6 +17,10 @@ _LOG_FILE="`basename $0 .sh`.log"
 ## 3 = dstudio-users-name,
 ## 4 = dstudio-user-password,
 ## 5 = dstudio-user-shortname
+## 6 = dstudio-hostname
+## 7 = dstudio-group
+##
+## and dstudio-users-hidden-status is set to NO by default.
 ## ------------------------------------------------------------
 
 read_csv_file() {
@@ -25,12 +29,18 @@ read_csv_file() {
       gsub(/"* *, *"*/, ","); # simplify field separation
       gsub(/^ *" */, "");     # strip leading space and quote
       gsub(/"* *$/, "");      # strip trailing space and quote
-      printf("defaults write %s/%s \"{", _db_byhost, $1);
+      printf("defaults write %s/%s ", _db_byhost, $1);
+      printf(" dstudio-users \047{");
       printf(" dstudio-users-admin-status = %s;", $2);
+      printf(" dstudio-users-hidden-status = NO;");
       printf(" dstudio-users-name = %s;", $3);
       printf(" dstudio-user-password = \"%s\";", $4);
       printf(" dstudio-user-shortname = %s;", $5);
-      printf(" }\"\n");
+      printf(" }\047\n");
+      printf("defaults write %s/%s ", _db_byhost, $1);
+      printf(" dstudio-hostname  \047%s\047\n", $6);
+      printf("defaults write %s/%s ", _db_byhost, $1);
+      printf(" dstudio-group  \047%s\047\n", $7);
    }'
 }
 
@@ -38,7 +48,7 @@ printf "\nStarting: `date`\n" | tee -a ${_LOG_FILE}
 for f in $_CSV_FILES ; do
     if test -s $f ; then
 	printf "\n Processing $f :\n"
-	cat $f | read_csv_file | /bin/sh
+	cat $f | read_csv_file #| /bin/sh
 	printf " Completed processing $f\n"
     else
 	printf " Error - input file $f either does not exist or is empty.\n"
